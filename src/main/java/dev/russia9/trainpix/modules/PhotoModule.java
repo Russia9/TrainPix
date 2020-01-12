7d16467b10ba11069c5a4a9c272d2b29f83f3a9a
Russia9 <russia9@russia9.dev>
2020-01-12 13:23:55 +0300

    Photo module rewrite

--- src/main/java/dev/russia9/trainpix/modules/PhotoModule.java
+++ src/main/java/dev/russia9/trainpix/modules/PhotoModule.java
@@ -53,105 +53,111 @@ public class PhotoModule implements BotModule {
             logger.trace("Detected LANG:" + lang);
 
             try {
-                String searchUrl = "https://trainpix.org/search.php?&order=3&num=" + URLEncoder.encode(searchQuery, "UTF-8");
+                String searchUrl = "https://trainpix.org/vsearch.php?&order=2&strict=on&num=" + URLEncoder.encode(searchQuery, "UTF-8");
                 Document document = getPage(searchUrl, lang);
 
-                Elements photos = document.getElementsByClass("x");
-                if (photos.size() > 0) {
-                    Element photo = photos.get(0);
-                    String photoPageLink = "https://trainpix.org" + photo.child(0).attr("href");
+                String trainLink = "https://trainpix.org/" + document.getElementsByClass("main").get(0)
+                        .getElementsByTag("tbody").get(0)
+                        .getElementsByAttributeValueStarting("class", "s").get(0)
+                        .getElementsByTag("a").get(0)
+                        .attr("href");
+                Document train = getPage(trainLink, lang);
 
-                    Document photoPage = getPage(photoPageLink, lang);
-                    String photoLink = "https://trainpix.org" + photoPage.getElementById("ph").attr("src");
-                    String authorName = photoPage.getElementsByClass("cmt_aname").get(0).getElementsByTag("a").text();
-                    String trainLink = "https://trainpix.org" + photoPage.getElementsByClass("pwrite").get(0).child(0).attr("href");
-                    Document trainPage = getPage(trainLink, lang);
+                String photoLink = "https://trainpix.org" + train.getElementsByClass("f").get(0).parent().attr("href");
+                Document photo = getPage(photoLink, lang);
 
-                    // Build date detection
-                    Elements built = trainPage.getElementsContainingOwnText(localeManager.getString(lang, "train.built"));
-                    String buildDate = localeManager.getString(lang, "train.built") + " " + localeManager.getString(lang, "unknown");
+                String photoImage = "https://trainpix.org" + photo.getElementById("ph").attr("src");
 
-                    if (!built.isEmpty() && built.parents().get(0).children().size() > 0) {
-                        buildDate = localeManager.getString(lang, "train.built") + " " + built.parents().get(0).getElementsByTag("b").text();
-                    }
+                String trainName = train.getElementsByTag("h1").get(0).text();
 
-                    // Depot detection
-                    Elements depot = trainPage.getElementsContainingOwnText(localeManager.getString(lang, "train.depot"));
-                    String depotName = localeManager.getString(lang, "train.depot") + " " + localeManager.getString(lang, "unknown");
+                String authorName = photo.getElementsByClass("cmt_aname").get(0).getElementsByTag("a").text();
 
-                    if (!depot.isEmpty() && depot.parents().get(0).children().size() > 0) {
-                        depotName = depot.parents().get(0).getElementsByTag("a").text();
-                    }
+                // Build date detection
+                Elements built = train.getElementsContainingOwnText(localeManager.getString(lang, "train.built"));
+                String buildDate = localeManager.getString(lang, "train.built") + " " + localeManager.getString(lang, "unknown");
 
-                    // Road detection
-                    Elements road = trainPage.getElementsContainingOwnText(localeManager.getString(lang, "train.road"));
-                    String roadName = localeManager.getString(lang, "train.road") + " " + localeManager.getString(lang, "unknown");
-
-                    if (!road.isEmpty() && road.parents().get(0).children().size() > 0) {
-                        roadName = road.parents().get(0).getElementsByTag("a").text();
-                    }
-
-                    // Category detection
-                    Elements category = trainPage.getElementsContainingOwnText(localeManager.getString(lang, "train.category"));
-                    String categoryName = "Other";
-
-                    if (!category.isEmpty() && road.parents().get(0).children().size() > 0) {
-                        categoryName = category.parents().get(0).child(1).text();
-                    }
-
-                    // Condition and color detection
-                    Color color = new Color(220, 220, 220);
-                    Element state = photoPage.getElementsByClass("state").first();
-                    String stateText = state.text();
-                    if ("ru".equals(lang)) {
-                        if (stateText.contains("Новый")) {
-                            color = new Color(108, 220, 53);
-                        }
-                        if (stateText.contains("Списан")) {
-                            color = new Color(220, 122, 110);
-                        }
-                        if (stateText.contains("Не работает")) {
-                            color = new Color(220, 217, 76);
-                        }
-                        if (stateText.contains("Нынешнее местонахождение и судьба неизвестны")) {
-                            color = new Color(220, 138, 104);
-                        }
-                        if (stateText.contains("Памятник/музейный экспонат/тренажёр")) {
-                            color = new Color(220, 53, 37);
-                        }
-                    } else {
-                        if (stateText.contains("New")) {
-                            color = new Color(108, 220, 53);
-                        }
-                        if (stateText.contains("Written off")) {
-                            color = new Color(220, 122, 110);
-                        }
-                        if (stateText.contains("Out of order")) {
-                            color = new Color(220, 217, 76);
-                        }
-                        if (stateText.contains("Current location and condition are unknown")) {
-                            color = new Color(220, 138, 104);
-                        }
-                        if (stateText.contains("Monument/Museum exhibit/Trainer")) {
-                            color = new Color(220, 53, 37);
-                        }
-                    }
-
-                    reply.setAuthor(searchQuery, trainLink, Reference.botImageLink);
-                    reply.addField(roadName + " | " + depotName, categoryName + " | " + stateText);
-                    reply.setImage(photoLink);
-                    reply.setColor(color);
-                    reply.setFooter(authorName + " | " + buildDate);
-                } else { // 404
-                    reply.setAuthor(Reference.botName);
-                    reply.setTitle(localeManager.getString(lang, "errors.404.header"));
-                    reply.addField(localeManager.getString(lang, "errors.404.title"), localeManager.getString(lang, "errors.404.description"));
+                if (!built.isEmpty() && built.parents().get(0).children().size() > 0) {
+                    buildDate = localeManager.getString(lang, "train.built") + " " + built.parents().get(0).getElementsByTag("b").text();
                 }
+
+                // Depot detection
+                Elements depot = train.getElementsContainingOwnText(localeManager.getString(lang, "train.depot"));
+                String depotName = localeManager.getString(lang, "train.depot") + " " + localeManager.getString(lang, "unknown");
+
+                if (!depot.isEmpty() && depot.parents().get(0).children().size() > 0) {
+                    depotName = depot.parents().get(0).getElementsByTag("a").text();
+                }
+
+                // Road detection
+                Elements road = train.getElementsContainingOwnText(localeManager.getString(lang, "train.road"));
+                String roadName = localeManager.getString(lang, "train.road") + " " + localeManager.getString(lang, "unknown");
+
+                if (!road.isEmpty() && road.parents().get(0).children().size() > 0) {
+                    roadName = road.parents().get(0).getElementsByTag("a").text();
+                }
+
+                // Category detection
+                Elements category = train.getElementsContainingOwnText(localeManager.getString(lang, "train.category"));
+                String categoryName = "Other";
+
+                if (!category.isEmpty() && road.parents().get(0).children().size() > 0) {
+                    categoryName = category.parents().get(0).child(1).text();
+                }
+
+                // Condition and color detection
+                Color color = new Color(220, 220, 220);
+                Element state = photo.getElementsByClass("state").first();
+                String stateText = state.text();
+                if ("ru".equals(lang)) {
+                    if (stateText.contains("Новый")) {
+                        color = new Color(108, 220, 53);
+                    }
+                    if (stateText.contains("Списан")) {
+                        color = new Color(220, 122, 110);
+                    }
+                    if (stateText.contains("Не работает")) {
+                        color = new Color(220, 217, 76);
+                    }
+                    if (stateText.contains("Нынешнее местонахождение и судьба неизвестны")) {
+                        color = new Color(220, 138, 104);
+                    }
+                    if (stateText.contains("Памятник/музейный экспонат/тренажёр")) {
+                        color = new Color(220, 53, 37);
+                    }
+                } else {
+                    if (stateText.contains("New")) {
+                        color = new Color(108, 220, 53);
+                    }
+                    if (stateText.contains("Written off")) {
+                        color = new Color(220, 122, 110);
+                    }
+                    if (stateText.contains("Out of order")) {
+                        color = new Color(220, 217, 76);
+                    }
+                    if (stateText.contains("Current location and condition are unknown")) {
+                        color = new Color(220, 138, 104);
+                    }
+                    if (stateText.contains("Monument/Museum exhibit/Trainer")) {
+                        color = new Color(220, 53, 37);
+                    }
+                }
+
+                reply.setTitle(trainName);
+                reply.setAuthor(searchQuery, trainLink, Reference.botImageLink);
+                reply.addField(roadName + " | " + depotName, categoryName + " | " + stateText);
+                reply.setImage(photoImage);
+                reply.setColor(color);
+                reply.setFooter(authorName + " | " + buildDate);
             } catch (IOException e) {
-                logger.warn(e);
+                logger.trace(e);
                 reply.setAuthor(Reference.botName);
                 reply.setTitle(localeManager.getString(lang, "errors.500.header"));
                 reply.addField(localeManager.getString(lang, "errors.500.title"), localeManager.getString(lang, "errors.500.description"));
+            } catch (IndexOutOfBoundsException e) {
+                logger.trace(e);
+                reply.setAuthor(Reference.botName);
+                reply.setTitle(localeManager.getString(lang, "errors.404.header"));
+                reply.addField(localeManager.getString(lang, "errors.404.title"), localeManager.getString(lang, "errors.404.description"));
             }
 
             event.getChannel().sendMessage(reply);
